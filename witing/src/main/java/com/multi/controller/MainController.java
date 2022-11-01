97480056ef1ab1c8f09cfd19f10c426af5d3fe11
627km <rudals_jeon@naver.com>
2022-11-01 16:18:29 +0900

    mypage, custdetail

--- witing/src/main/java/com/multi/controller/MainController.java
+++ witing/src/main/java/com/multi/controller/MainController.java
@@ -1,15 +1,50 @@
 package com.multi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.multi.dto.CustDTO;
+import com.multi.service.CustService;
+
 @Controller
 public class MainController {
 
+	@Autowired
+	CustService custservice;
+	
 	@RequestMapping("/main")
 	public String main(Model model) {
 		model.addAttribute("center","maincenter");
 		return "index";
 	}
+	
+	@RequestMapping("/mypage")
+	public String mypage(Model model) {
+		CustDTO cust = null;
+		try {
+			cust = custservice.get("kmj");
+			model.addAttribute("mypage", cust);
+			model.addAttribute("center","mypage");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "index";
+	}
+	@RequestMapping("/custdetail")
+	public String custdetail(Model model) {
+		CustDTO cust = null;
+		try {
+			cust = custservice.get("kmj");
+			model.addAttribute("custdetail", cust);
+			model.addAttribute("center","custdetail");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "index";
+	}
+	
+	
+	
 }
--- witing/src/main/resources/static/js/main.js
+++ witing/src/main/resources/static/js/main.js
@@ -14,7 +14,7 @@
 
 	var fullHeight = function() {
 
-		$('.js-fullheight').css('height', $(window).height());
+		$('.js-fullheight').css('height', $(window).height()*0.5);
 		$(window).resize(function(){
 			$('.js-fullheight').css('height', $(window).height());
 		});
