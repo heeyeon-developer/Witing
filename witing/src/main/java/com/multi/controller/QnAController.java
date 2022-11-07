56740faf5b14c1435472ff7d5e422b727f4621e7
627km <rudals_jeon@naver.com>
2022-11-07 14:33:15 +0900

    qna add

--- witing/src/main/java/com/multi/controller/QnAController.java
+++ witing/src/main/java/com/multi/controller/QnAController.java
@@ -2,14 +2,19 @@ package com.multi.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.multi.dto.CustDTO;
+import com.multi.dto.HotelDTO;
 import com.multi.dto.PostDTO;
 import com.multi.service.CustService;
+import com.multi.service.HotelService;
 import com.multi.service.PostService;
 
 @Controller
@@ -18,17 +23,44 @@ public class QnAController {
 	CustService custservice;
 	@Autowired
 	PostService postservice;
+	@Autowired
+	HotelService hotelservice;
 	
 	@RequestMapping("/qna")
-	public String main(Model model, String custid) {
+	public String qna(Model model, String custid) {
 		System.out.println(custid);
 		try {
 			CustDTO cust = custservice.get(custid);
 			List<PostDTO> list = postservice.myqna(custid);
 			System.out.println(list);
 			model.addAttribute("qna", cust);
-			model.addAttribute("myqna", list);
+			model.addAttribute("list", list);
 			model.addAttribute("center", "qna");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "index";
+	}
+	
+	@RequestMapping("/qnadetail")
+	public String qnadetail(Model model, int postid) {
+		try {
+			PostDTO post = postservice.get(postid);
+			model.addAttribute("qnadetail", post);
+			model.addAttribute("center", "qnadetail");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "index";
+	}
+	
+	@RequestMapping("/write_qna")
+	public String write_qna(Model model, Integer hotelid) {
+
+		try {
+			HotelDTO hotel = hotelservice.get(hotelid);
+			model.addAttribute("hotel", hotel);
+			model.addAttribute("center", "write_qna");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
