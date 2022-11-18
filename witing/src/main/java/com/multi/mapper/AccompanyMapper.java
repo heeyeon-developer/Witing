40efbf4e7ac7775b94ff34fcf77b25e379721dcf
627km <rudals_jeon@naver.com>
2022-11-18 17:56:10 +0900

    mypage fix

--- witing/src/main/java/com/multi/mapper/AccompanyMapper.java
+++ witing/src/main/java/com/multi/mapper/AccompanyMapper.java
@@ -6,12 +6,12 @@ import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 import com.multi.dto.AccompanyDTO;
-import com.multi.dto.Criteria;
+//import com.multi.dto.Criteria;
 import com.multi.frame.MyMapper;
 
 @Repository
 @Mapper
 public interface AccompanyMapper extends MyMapper<Integer, AccompanyDTO> {
 	public void accomdel(int accomid) throws Exception;
-	public List<AccompanyDTO> selectpaging(Criteria criteria);
+//	public List<AccompanyDTO> selectpaging(Criteria criteria);
 }
