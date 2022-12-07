a4a4b3838c1e99c4ab5740530d5e3fd0e9f48ff2
heeyeonkim <heeyeon3472@gmail.com>
2022-12-07 16:34:22 +0900

    hotel add, style.css add

--- witing/src/test/java/com/multi/Cust/RegisterCust.java
+++ witing/src/test/java/com/multi/Cust/RegisterCust.java
@@ -20,7 +20,7 @@ class RegisterCust {
 	void contextLoads() {
 		CustDTO cust = new CustDTO("hytest3", "hy123", "희연테스트3", Date.valueOf("1994-11-11"), "010-1234-1234", "rudals0627@gmail.com", "제주시", "1층", 12345, null, "woman", 0, "Korea",null,null);
 		try {
-//			service.register(cust);
+			service.register(cust);
 			List<CustDTO> list = service.getall();
 			for(CustDTO c : list)
 				System.out.println(c);
